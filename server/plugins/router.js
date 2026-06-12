9f1f322277ee535f1bea9af34f9eb12dd2426b60
Scott <scott.dormand@defra.gov.uk>
2026-06-12 11:59:26 +0100

    update router back to views

--- server/plugins/router.js
+++ server/plugins/router.js
@@ -2,7 +2,7 @@ import constants from '../utils/constants.js'
 
 const router = async () => {
   const routes = [].concat(
-    ...await Promise.all(Object.values(constants.views).map(async route => (await import(`../routes/${routes}.js`)).default))
+    ...await Promise.all(Object.values(constants.routes).map(async route => (await import(`../routes/${routes}.js`)).default))
   )
 
   return {
